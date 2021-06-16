4cfc04b12be07c3f1b0ecb43e4637d75b73c1564
Your Name <you@example.com>
2021-06-16 17:08:30 +0200

    prg mié 16 jun 2021 17:08:30 CEST

--- docs/assets/javascripts/config.js
+++ docs/assets/javascripts/config.js
@@ -1,22 +1,19 @@
 
 
-onload = function () {
-
-    document.getElementsByClassName('md-source__icon md-icon')[0].style.visibility = 'hidden';
-    document.getElementsByClassName('md-footer-meta md-typeset')[0].style.visibility = 'hidden';
+document.getElementsByClassName('md-source__icon md-icon')[0].style.visibility = 'hidden';
 
 
-    window.MathJax = {
-        tex: {
-            inlineMath: [["\\(", "\\)"]],
-            displayMath: [["\\[", "\\]"]],
-            processEscapes: true,
-            processEnvironments: true
-        },
-        options: {
-            ignoreHtmlClass: ".*|",
-            processHtmlClass: "arithmatex"
-        }
-    };
-    console.log('ooooooS')
+
+window.MathJax = {
+    tex: {
+        inlineMath: [["\\(", "\\)"]],
+        displayMath: [["\\[", "\\]"]],
+        processEscapes: true,
+        processEnvironments: true
+    },
+    options: {
+        ignoreHtmlClass: ".*|",
+        processHtmlClass: "arithmatex"
+    }
 };
+// console.log('ooooooS')
